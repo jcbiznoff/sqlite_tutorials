351021685a4aab4eebc1183ac287bf7496a2a26c
Hyunjin Chung <hyunjin.chung@lge.com>
2014-12-17 17:26:58 +0900

    opendb_tut1.c updated

--- opendb_tut1.c
+++ opendb_tut1.c
@@ -1,22 +1,22 @@
-//include sqlite3
-//open connection to db
-//if error, exit program
-//else print success message
-//close connection to db
-//exit program
-//
-//
+/**
+ * Summary: Simple database open/close
+ * Create/Open a database, print a statement,
+ * then close connection to it.
+ *
+ * Author: Hyunjin Chung
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h>
 
+#define DBNAME "./wargame.db"
+
 int main(int argc, char **argv)
 {
-    //1. open connection to db
-    sqlite3 * db= NULL;
-    const char * dbName = "./jaygame.db";
-    if( SQLITE_OK != sqlite3_open(dbName, &db)){
+    //1. open connection to db.
+    sqlite3 * db = NULL;
+    if( SQLITE_OK != sqlite3_open(DBNAME, &db)){
         printf("db open failed...\n");
         printf("error message is: %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
@@ -24,9 +24,9 @@ int main(int argc, char **argv)
     }
 
     //2. db connection is good
-    //call prepare now
-    printf("db open/create success..\b");
+    printf("db open/create success..\n");
 
+    //3. close connection to db.
     sqlite3_close(db);
     return 0;
 }
